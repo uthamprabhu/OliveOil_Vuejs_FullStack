ce741d8bb9c5eba713d06eb07f3a5d4e3cda3aef
uthamprabhu <uthamprabhu1@gmail.com>
2023-10-30 21:29:03 +0530

    noiceeee

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -83,7 +83,7 @@ async function start() {
     const port = process.env.PORT || 8000
 
     app.listen(port, () => {
-        console.log(`Server is listening on http://localhost:${port}`)
+        console.log('Server is listening on port ' + port)
     })
 }
 
